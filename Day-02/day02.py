3991cac5801513cf9fc1800f48f1f73b41155d6a
OMPRAKASH <oom880240@gmail.com>
2026-07-17 20:21:14 +0530

    Day 02: Learn input(), int(), float(), and basic calculation programs

---
+++ Day-02/day02.py
@@ -0,0 +1,39 @@
+a = 10
+b = 20
+c = a+b
+print ("Addition:",c)
+
+a = input("Enter a number : ")
+b = input("Enter b number : ")
+c = a+b
+print ("Addition :",c)
+
+a = int (input("Enter a number : "))
+b = int (input("Enter b number : "))
+c = a+b
+print ("Addition :",c)
+
+# WAP to calculate the gross salary of an employee where
+# Hint:- HAR is 20% an DA is 30% his basic salary + HRA + DA.
+
+basic_salary = float (input ("Enter your Basic Salary : "))
+har  =  bs*20/100 
+da = bs*30/100
+gross_salary = bs+har+da
+print ("Gross Salary : ", gross_salary )
+            
+# WAP to calculate the cube of a number by user input.
+# Random 
+area = int (input (" Enter a number : "))
+print ( " Cube of a number:", area * area * area )
+
+# Proper way
+num = int (input ("Enter a number:"))
+cube = num * num * num
+print ("cube : ", cube) 
+           
+# WAP a programe to caculate the area of circle
+# Hint:- AREA = 3.14 * radius * radius
+
+radius = float (input("Enter a radius : "))
+print ( " Area of Circle =",  3.14 * radius * radius )
